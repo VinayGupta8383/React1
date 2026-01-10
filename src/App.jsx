1af923d9d29d1bd500af04df5560a308dee149b3
VinayGupta8383 <vinaygupta4584@gmail.com>
2026-01-10 22:04:24 +0530

    function call

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,23 @@
 import myPhoto from "./my_photo.JPG";
 
 function App() {
-    const name = "vinay"
-    const userobj = {
-      name: "vinay",
-      email:"vinay@gmail.com",  
-      age: 22,
-      city : "hyd"
-      
+    function callfun(){
+        alert("function called");
     }
-    const userArray = ["vinay", "ajay", "vijay"];
-    let age = 22;
-    let e = 32;
-    let path="./my_photo.jpg";
-    function friut(){
-      return "mango"
+    
+    const fruit=()=>{
+        alert("apple");
     }
-
-    function add(a,b){
-      return a+b;
-    }
-
-    function operation(a,b, op){
-      if(op == "+"){
-        return a+b;
-      }else if(op == "-"){
-        return a-b;
-      }else{
-        return "a*b"
-      }
-    }
+    
+    
   return (
     <div>
-      <h1>jsx with curly braces</h1>
-      <h1>{name}</h1>
-      <h1>{age+e}</h1>
-      <h1>{friut()}</h1>
-      <h1>{add(5,6)}</h1>
-      <h1>{userobj.name}</h1>
-      <h1>{userArray[1]}</h1>
-      <h1>{userobj.city}</h1>
-      <img 
-      src={myPhoto} alt="my photo"           
+        <h1>event and function call</h1>
+        <button onClick={fruit}>banana</button>
+        <button onClick={callfun}>Click Me</button>
+        
+                 
       
-      />
     </div>
     
   )
