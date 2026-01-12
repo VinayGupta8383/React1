ca6729f97b7daf41931c24858627cc5da196a024
VinayGupta8383 <vinaygupta4584@gmail.com>
2026-01-12 23:49:27 +0530

    multiple condition

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,24 @@
 import{ useState } from "react"
 
 function App() {
-  const [display,setDisplay] = useState(true)  
+  const [count,setCount]=useState(0);  
   
-  
-  
-
   return (
-    <>
-       <h1>Toggle in React js</h1>  
-       <button onClick={()=>setDisplay(!display)}>Toggle</button>        
+    <div>
+       <h1>{count}</h1>  
+       <button onClick={()=>setCount(count+1)}>Counter</button>        
        
-       {
-        display? <h1>vinay</h1>:<h1>no user name</h1>
-}
-      
-    </>
+       { 
+        count==0? <h1>Condition 0</h1>
+        :count==1? <h1>Condition 1</h1>
+        :count==2? <h1>Condition 2</h1>
+        :count==3? <h1>Condition 3</h1>
+        :count==4? <h1>Condition 4</h1>
+        :count==5? <h1>Condition 5</h1>
+        :<h1>other Condition</h1>
+        }
+    
+    </div>
     
   )
 }
