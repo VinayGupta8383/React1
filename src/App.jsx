6b86b96ce07a04b420765ffa24a5d18bf5623a8f
VinayGupta8383 <vinaygupta4584@gmail.com>
2026-01-15 12:47:52 +0530

    Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,53 @@
+import { useState } from "react";
 
-import{ useState } from "react"
+import College from "./College"
+import Student from "./Student"
+import User from "./User"
 
 function App() {
-  const [count,setCount]=useState(0);  
-  
+  // let userName = "vinay gupta"
+  // let age = 29
+  // let email = "vinay@example.com"
+  let userObject = {
+    name: "vinay gupta",
+    age: 29,
+    email: "vinay@example.com"
+  }
+
+  let userObject2 = {
+    name: "john doe",
+    age: 25,
+    email: "john@example.com"
+  }
+
+  let userObject3 = {
+    name: "jane smith",
+    age: 28,
+    email: "jane@example.com"
+  }
+
+  let collegeNames = ['harvard', 'mit', 'stanford']
+  const [student, setStudent] = useState()
+
   return (
     <div>
-       <h1>{count}</h1>  
-       <button onClick={()=>setCount(count+1)}>Counter</button>        
-       
-       { 
-        count==0? <h1>Condition 0</h1>
-        :count==1? <h1>Condition 1</h1>
-        :count==2? <h1>Condition 2</h1>
-        :count==3? <h1>Condition 3</h1>
-        :count==4? <h1>Condition 4</h1>
-        :count==5? <h1>Condition 5</h1>
-        :<h1>other Condition</h1>
-        }
-    
+       <h1>props in react</h1>
+       {student && <Student name="student"/>}
+       <button onClick={()=>setStudent("bhasker")}>Update student name</button>
+      {/* <User name={userName} age={age} email={email} /> */}
+      {/* <College name={collegeNames[0]} />
+      <College name={collegeNames[1]} />
+      <College name={collegeNames[2]} /> */}
+
+
+
+      <User user= {userObject} />
+      <User user= {userObject2} />
+      <User user= {userObject3} />
+      
     </div>
     
   )
 }
 
-export default App
+export default App 
---
+++ src/Student.jsx
@@ -0,0 +1,14 @@
+function Student({name}) {
+
+   
+    return (
+        <div>
+            <hr />
+            <h1> Student Name;{name}</h1>
+            <hr />
+        </div>
+    )
+}
+
+
+export default Student;
