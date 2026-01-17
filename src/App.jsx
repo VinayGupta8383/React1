95572b6be80edf023cae1acb9430dc6e2ffe2772
VinayGupta8383 <vinaygupta4584@gmail.com>
2026-01-17 10:37:22 +0530

    get input field value

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,14 @@
-import Wrapper from "./Wrapper";
-// import User from "./User";
+import { useState } from 'react';                    
+
 function App() {
-  
+  const [val,setVal]=useState("vinay gupta")
 
   return (
     <div>
-       <h1>Props in React</h1>
-       <Wrapper>
-        <h1>hello vinay</h1>
-       </Wrapper>
-
-
-       <Wrapper>
-        <h1>hello bro</h1>
-       </Wrapper>
-
-
-       <Wrapper>
-        <h1>hello</h1>
-       </Wrapper>
-
-       {/* <User name="vinay"/>
-       <User name="john"/>
-       <User />
-       <User />
-       <User /> */}
-
+       <h1>Get Input field Value</h1>
+       <input type="text" value={val} onChange={(event)=>setVal(event.target.value)} placeholder="Enter Name" />  
+       <h1>{val}</h1> 
+       <button onClick={()=>setVal("")}>Clear value</button>    
     </div>
     
   )
