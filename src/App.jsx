c7bf4b4b504d5437bdd6b51c71bdcf8cc2ba32ef
VinayGupta8383 <vinaygupta4584@gmail.com>
2026-01-20 22:34:06 +0530

    reuse component in loop

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
                 
-
-
+import User from "./User";
 function App() {
    
 
@@ -36,88 +35,15 @@ function App() {
   
   return (
     <div>
-      <h1>Loop in JSX with Map Function</h1>
-      <table border="1">
-    <thead>
-       <tr>
-        <td>Id</td>
-        <td>Name</td>
-        <td>Email</td>
-        <td>Age</td>
-        </tr>
-    </thead>
-       <tbody>
-        {
-          userData.map((user)=>(
-        <tr key={user.id} >
-        <td>{user.id}</td>
-        <td>{user.name}</td>
-        <td>{user.email}</td>
-        <td>{user.age}</td>
-        </tr>
-          ))
-        }
-        </tbody>    
-
-      </table>
-
-      <h1>Dummy Data</h1>
-      <table border="1">
-         <thead>
-            <tr>
-              <td>Id</td>
-              <td>Name</td>
-              <td>Email</td>
-              <td>Age</td>
-            </tr>
-         </thead>
-         <tbody>
-            <tr>
-              <td>1</td>
-              <td>vinay</td>
-              <td>vinay434@test.com</td>
-              <td>28</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>vinay</td>
-              <td>vinay434@test.com</td>
-              <td>28</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>vinay</td>
-              <td>vinay434@test.com</td>
-              <td>28</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>vinay</td>
-              <td>vinay434@test.com</td>
-              <td>28</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>vinay</td>
-              <td>vinay434@test.com</td>
-              <td>28</td>
-            </tr>
-
-            <tr>
-              <td>1</td>
-              <td>vinay</td>
-              <td>vinay434@test.com</td>
-              <td>28</td>
-            </tr>
-               
-         </tbody>
-      </table>
-
-    </div>
+      <h1>Reuse Component in Loop</h1>
+      {
+       userData.map((user)=>(
+       <div key={user.id}>   
+        <User data={user}/>
+       </div> 
+       )) 
+      }
+       </div>
     
   )
 }
--- src/User.jsx
+++ src/User.jsx
@@ -1,10 +1,16 @@
-function User({name="new user"}) {
-    
-    return (
-        <div>
-            <h1>Hi, {name}</h1>
-        </div>
-    ) 
-
+const User = ({data})=>{
+    return(
+        <div style={{
+            border: "1px solid green",
+            padding: "10px",
+            margin: "10px",
+            width: "400px",
+            bordeRadius:"10px"
+        }}>
+            <h3>Name: <span style={{color:'green'}}>{data.name}</span></h3>
+            <h3>Age: <span style={{color:'green'}}>{data.age}</span></h3>
+            <h3>Email: <span style={{color:'green'}}>{data.email}</span></h3>
+        </div>    
+    )
 }
-export default User;  
+export default User; 
