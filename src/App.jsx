04ff082b5beac1228a26cd349e90bb7cdcf73368
VinayGupta8383 <vinaygupta4584@gmail.com>
2026-02-28 00:23:08 +0530

    useref hook in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button, Alert } from "react-bootstrap";
+import { useRef } from "react";
 
 function App() {
+  const inputRef = useRef(null);
+
+  const inputHandler = () => {
+    console.log(inputRef);
+    inputRef.current.focus();
+    inputRef.current.style.color = "red";
+    inputRef.current.placeholder = "enter password";
+    inputRef.current.value = "123";
+  };
+
+  const toggleHandler = () => {   // ✅ name fixed
+    if (inputRef.current.style.display !== "none") {
+      inputRef.current.style.display = "none";
+    } else {
+      inputRef.current.style.display = "inline";
+    }
+  };
+
   return (
     <>
-      <h1>Add Bootstrap in React js</h1>
-      <Alert variant="success">Hello, BT installed</Alert>
-      <Button onClick={()=>AudioListener("Bootstrap Button")} variant="danger">Bootstrap Button</Button>
-      <Button variant="success">OK</Button>
-      <Button variant="warning">OK</Button>
-      <Button onClick={()=>AudioListener("Bootstrap Button")} variant="danger">Simple Button</Button>
+      <h1>useRef</h1>
+      <button onClick={toggleHandler}>Toggle</button>
+      <input ref={inputRef} type="text" placeholder="Enter user name" />
+      <button onClick={inputHandler}>Focus on Input field</button>
     </>
   );
 }
