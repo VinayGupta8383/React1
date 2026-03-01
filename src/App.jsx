2600550c7a416e011d9758c1c92d3cc71ea152de
VinayGupta8383 <vinaygupta4584@gmail.com>
2026-03-01 12:08:24 +0530

    update app and user component

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,20 @@
-import { useRef } from "react";
-
+import User from "./User"
 function App() {
-
-  const userRef = useRef(null);
-  const passwordRef = useRef(null);
-
-  const handleForm = (event) => {
-    event.preventDefault();
-    const user = document.querySelector("#user").value;
-    const password = document.querySelector("#password").value;
-    console.log("Without Ref:", user, password);
-  };
-
-  const handleFormRef = (event) => {
-    event.preventDefault();
-    console.log("With Ref:", userRef.current.value, passwordRef.current.value);
-  };
-
-  return (
-    <>
-      <h1>Uncontrolled Component</h1>
-      <form onSubmit={handleForm}>
-        <input type="text" id="user" placeholder="enter user name" />
-        <br /><br/>
-        <input type="password" id="password" placeholder="enter user password" />
-        <br /><br />
-        <button>Submit</button>
-      </form>
-
-      <hr />
-
-      <h1>Uncontrolled Component with userRef</h1>
-      <form onSubmit={handleFormRef}>
-        <input type="text" ref={userRef} placeholder="enter user name" />
-        <br /><br/>
-        <input type="password" ref={passwordRef} placeholder="enter user password" />
-        <br /><br />
-        <button>Submit with ref</button>
-      </form>
-    </>
-  );
+  const displayName=(name)=>{
+    alert(name)
+  }
+  const getUser=()=>{
+    alert("get user function called")
+  }
+    return (
+      <>
+        <h1>Call Parent component Function from child component</h1>
+        <User displayName={displayName} name="anil" getUser={getUser} />
+        <User displayName={displayName} name="sam" getUser={getUser} />
+        <User displayName={displayName} name="peter" getUser={getUser} />
+        <User displayName={displayName} name="bruce" getUser={getUser} />
+      </>
+    )
 }
 
 export default App;
